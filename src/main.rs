a27fcd372044cb4a998f9d8a39a1959e600e6719
Joakim Riikonen <joakim.riiikonen@gmail.com>
2024-03-08 09:20:29 +0200

    Add simple rendering of a sphere

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() {
 
     let focal_length = 1.0;
     let viewport_height = 2.0;
-    let viewport_width = viewport_height * (image_width / image_height) as f64;
+    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
     let camera_center = Point3::new(0.0, 0.0, 0.0);
 
     // Calculate vectors across the horizontal and down the vertical viewport edges
@@ -32,8 +32,8 @@ fn main() {
     // Calculate location of upper-left pixel
     let viewport_upper_left = &camera_center
         - &Vec3::new(0.0, 0.0, focal_length)
-        - &viewport_u
-        - &viewport_v;
+        - &viewport_u / 2.0
+        - &viewport_v / 2.0;
     let pixel00_loc = viewport_upper_left + (&pixel_delta_u + &pixel_delta_v) * 0.5;
 
     let mut output = format!("P3\n{} {}\n255\n", image_width, image_height);
@@ -44,7 +44,7 @@ fn main() {
             let pixel_center = &pixel00_loc + &(&pixel_delta_u * i as f64) + (&pixel_delta_v * j as f64);
             let ray_direction = &pixel_center - &camera_center;
 
-            let r = Ray::new(pixel_center, ray_direction);
+            let r = Ray::new(&camera_center, &ray_direction);
 
             let pixel_color = ray_color(&r);
 
@@ -59,7 +59,21 @@ fn main() {
 }
 
 fn ray_color(ray: &Ray) -> Color {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, ray) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
+
     let unit_direction = ray.direction.unit_vector();
     let a = 0.5 * (unit_direction.y + 1.0);
     Color::new(1.0, 1.0, 1.0) * (1.0 - a) + Color::new(0.5, 0.7, 1.0) * a
+}
+
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin - &center;
+    let a = r.direction.dot(&r.direction);
+    let b = oc.dot(&r.direction) * 2.0;
+    let c = oc.dot(&oc) - radius*radius;
+    let discriminant = b*b - 4.0*a*c;
+
+    return discriminant >= 0.0;
 }
--- src/ray.rs
+++ src/ray.rs
@@ -1,16 +1,16 @@
 use crate::vec3::{Point3, Vec3};
 
-pub struct Ray {
-  pub origin: Point3,
-  pub direction: Vec3,
+pub struct Ray<'a> {
+  pub origin: &'a Point3,
+  pub direction: &'a Vec3,
 }
 
-impl Ray {
-  pub fn new(origin: Point3, direction: Vec3) -> Ray {
+impl Ray<'_> {
+  pub fn new<'a>(origin: &'a Point3, direction: &'a Vec3) -> Ray<'a> {
     Ray { origin, direction }
   }
 
   pub fn at(&self, t: f64) -> Point3 {
-    &self.origin + &(&self.direction * t)
+    self.origin + &(self.direction * t)
   }
 }
